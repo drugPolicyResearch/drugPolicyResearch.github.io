c571746bf5d7424624fffa3c2d95655510b63a2c
myra <myraramdenbourg@gmail.com>
2019-04-15 23:34:38 -0500

    added social process stuff

--- js/index.js
+++ js/index.js
@@ -140,3 +140,13 @@
 jQuery(function(){
 	portfolio.init();
 });
+
+
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
